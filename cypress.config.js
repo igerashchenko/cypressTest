aaadbb5fbed974d9e3b2f7e58fe3d87958832d7a
Iryna Herashchenko <igerashchenko2@gmail.com>
2025-03-25 17:43:43 +0100

    Setup reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: 'g7q3ms',
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: 'https://example.cypress.io'
   },
