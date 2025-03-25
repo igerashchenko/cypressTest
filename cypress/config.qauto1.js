1dbd913992536ae1cad420232dd2056f0aea805f
Iryna Herashchenko <igerashchenko2@gmail.com>
2025-03-25 17:34:20 +0100

    Created config files

---
+++ cypress/config.qauto1.js
@@ -0,0 +1,7 @@
+module.exports = {
+    baseUrl: 'https://qauto.forstudy.space/',
+    users: {
+        email: 'user1@iryna.com',
+        password: 'Password123!'
+    }
+  };
---
+++ cypress/config.qauto2.js
@@ -0,0 +1,7 @@
+module.exports = {
+    baseUrl: 'https://qauto2.forstudy.space/',
+    users: {
+        email: 'user2@iryna.com',
+        password: 'Password123!'
+    }
+  };
